28a53107d85d04cde3e3938256dcf5a247965a8f
YJ402 <uky0916@gmail.com>
2025-03-27 21:55:16 +0900

    [feat] 플레이어 상태머신 작성중

---
+++ Assets/01.Scripts/01.Player/PlayerLBStateMachine.cs
@@ -0,0 +1,26 @@
+public class PlayerLBStateMachine : StateMachine
+{
+    protected IState currentState; // IState 클래스를 넣을 칸(카세트 오디오를 넣을 공간)
+
+    public void ChangeState(IState state)
+    {
+        currentState?.Exit();
+        currentState = state; // 카세트 오디오 넣기
+        currentState?.Enter();
+    }
+
+    public void HanldeInput()
+    {
+        currentState.HandleInput();
+    }
+
+    public void Update()
+    {
+        currentState.Update();
+    }
+
+    public void PhysicsUpdate()
+    {
+        currentState.PhysicsUpdate();
+    }
+}
---
+++ Assets/01.Scripts/01.Player/PlayerLB_GroundedState.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLB_BaseState : IState
+{
+    public virtual void Enter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual void HandleInput()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual void PhysicsUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
