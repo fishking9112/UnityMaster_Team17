fb88f3aefbb2468dc49b4ddef10612a7ec26c31c
fishking9112 <fishking9112@gmail.com>
2025-03-27 22:28:30 +0900

    [Fix] 누가 GameManager를 만드는가.
    아무도 모른다

--- Assets/01.Scripts/00.Managers/UIManager.cs
+++ Assets/01.Scripts/00.Managers/UIManager.cs
@@ -7,12 +7,10 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameManager gameManager = GameManager.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
