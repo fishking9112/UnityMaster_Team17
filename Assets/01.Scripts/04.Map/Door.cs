edaef24d8438faaffa19369c0a056b5d956a81fd
ManJin12 <hh5570hh@naver.com>
2025-03-31 16:59:07 +0900

    [Feat] 문 추가 및 2개 셋터 배치 완료

---
+++ Assets/01.Scripts/04.Map/Door.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animation anim;
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animation>();
+    }
+    public void OpenDoor()
+    {
+        anim.Play("open");
+        //StartCoroutine(CloseDoor());
+    }
+
+    private IEnumerator CloseDoor()
+    {
+        yield return new WaitForSeconds(3f);
+        anim.Play("close");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.Play("close");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player is in the trigger");
+            anim.Play("open");
+        }
+    }
+}
