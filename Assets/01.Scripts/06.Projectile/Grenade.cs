34c0c2731c039d3e78f8bd7ddea75354c97045b6
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-04-01 21:30:22 +0900

    [Feat] 유탄 구현
    아이템으로 사용할 유탄 구현

---
+++ Assets/01.Scripts/06.Projectile/Grenade.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float explosionRange;
+
+    public GameObject explosionParticle;
+
+    private GameObject curParticle;
+
+    private void Explosion()
+    {
+        curParticle = Instantiate(explosionParticle, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRange);
+
+        List<Collider> enemyColliders = new List<Collider>();
+
+        foreach (var col in colliders)
+        {
+            if (col.CompareTag("Enemy"))
+            {
+                enemyColliders.Add(col);
+            }
+        }
+
+        foreach (var enemy in enemyColliders)
+        {
+            Vector3 direction = (enemy.transform.position - transform.position).normalized;
+
+            RaycastHit hit;
+
+            Debug.DrawRay(transform.position, direction * explosionRange, Color.red, 1f);
+
+            if (Physics.Raycast(transform.position, direction, out hit, explosionRange))
+            {
+                if (hit.collider.gameObject.CompareTag("Grenade_Target"))
+                {
+                    Debug.Log(hit.collider.name + " 피격");
+                }
+            }
+        }
+
+        gameObject.SetActive(false);
+        Invoke("DelayDestroy", 3f);
+    }
+
+    private void DelayDestroy()
+    {
+        Destroy(curParticle);
+        Destroy(this.gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRange);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Explosion();
+    }
+}
