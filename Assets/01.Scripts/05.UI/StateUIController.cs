eafb5dbc612c89e246f041bed93b43233ffe64a5
ManJin12 <hh5570hh@naver.com>
2025-04-02 21:55:01 +0900

    [Feat] : Hp Fill 추가, UI업데이트 추가

--- Assets/01.Scripts/05.UI/StateUIController.cs
+++ Assets/01.Scripts/05.UI/StateUIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StateUIController : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class StateUIController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI textGrenadeCount;
     [SerializeField] private TextMeshProUGUI textRepairCount;
     [SerializeField] private TextMeshProUGUI textAmorCount;
+    [SerializeField] private Image imageHP;
 
     void Start()
     {
@@ -16,10 +18,17 @@ public class StateUIController : MonoBehaviour
         textGrenadeCount.text = GameManager.Instance.player.Condition.CurGrenadeCount.ToString();
         textRepairCount.text = GameManager.Instance.player.Condition.CurRepairKitCount.ToString();
         textAmorCount.text = GameManager.Instance.player.Condition.CurMagazineCount.ToString();
+        float fillAmount = Mathf.Clamp((float)GameManager.Instance.player.Condition.CurHealth / (float)GameManager.Instance.player.Condition.MaxHealth, 0f, 1f);
+        imageHP.fillAmount = fillAmount;
     }
 
     void Update()
     {
-        
+        textHP.text = GameManager.Instance.player.Condition.CurHealth.ToString();
+        textGrenadeCount.text = GameManager.Instance.player.Condition.CurGrenadeCount.ToString();
+        textRepairCount.text = GameManager.Instance.player.Condition.CurRepairKitCount.ToString();
+        textAmorCount.text = GameManager.Instance.player.Condition.CurMagazineCount.ToString();
+        float fillAmount = Mathf.Clamp((float)GameManager.Instance.player.Condition.CurHealth / (float)GameManager.Instance.player.Condition.MaxHealth, 0f, 1f);
+        imageHP.fillAmount = fillAmount;
     }
 }
