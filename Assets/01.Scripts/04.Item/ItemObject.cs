5a6cdd2424402ffdf2af125af8af75e436a9d772
ManJin12 <hh5570hh@naver.com>
2025-04-02 19:12:11 +0900

    [Feat] : 아이템 재생성 추가, GameScene 추가

--- Assets/01.Scripts/04.Item/ItemObject.cs
+++ Assets/01.Scripts/04.Item/ItemObject.cs
@@ -1,6 +1,10 @@
+using UnityEngine;
+
 public class ItemObject : InteractableObject
 {
     public ItemData data;
+
+    public float respawnTime;
 
     public override string GetNameText()
     {
@@ -12,20 +16,35 @@ public class ItemObject : InteractableObject
         return data.itemDescription;
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            OnInteract();
+        }
+    }
     public override void OnInteract()
     {
-        switch (data.itemType)
-        {
-            case ItemType.GRENADE:
-                //GameManager.Instance.player.Condition.AddGrenade(data.value);
-                break;
-            case ItemType.MAGAZINE:
-                //GameManager.Instance.player.Condition.AddMagazine(data.value);
-                break;
-            case ItemType.REPAIR_KIT:
-                //GameManager.Instance.player.Condition.AddRepairKit(data.value);
-                break;
-        }
-        Destroy(this.gameObject);
+        //switch (data.itemType)
+        //{
+        //    case ItemType.GRENADE:
+        //        //GameManager.Instance.player.Condition.AddGrenade(data.value);
+        //        break;
+        //    case ItemType.MAGAZINE:
+        //        //GameManager.Instance.player.Condition.AddMagazine(data.value);
+        //        break;
+        //    case ItemType.REPAIR_KIT:
+        //        //GameManager.Instance.player.Condition.AddRepairKit(data.value);
+        //        break;
+        //}
+
+        this.gameObject.SetActive(false);
+
+        Invoke("RespawnItem", respawnTime);
+    }
+
+    private void RespawnItem()
+    {
+        this.gameObject.SetActive(true);
     }
 }
